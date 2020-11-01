baea3610b8efc628e47040537d613cab1977bdcf
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-01 21:13:39 +0200

    Initial Commit

---
+++ MapDirections/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapDirections
+//
+//  Created by Ramill Ibragimov on 01.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
